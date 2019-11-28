2f8b75475642a7717faa78a2b9401ca405190c5f
Yanglin Zhao <yz1876@gmail.com>
2019-11-28 12:05:22 -0500

    Get frames

--- src/detect.py
+++ src/detect.py
@@ -1,2 +1,14 @@
+import cv2
+
+
+def get_frames(video_file):
+    cap = cv2.VideoCapture(video_file)
+    while(True):
+        has_frame, frame = cap.read()
+        print(frame)
+        if not has_frame:
+            break
+
+
 def get_end_credit(video_file):
-    return 123
+    frames = get_frames(video_file)
